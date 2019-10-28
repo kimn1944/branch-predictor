2f5712c8edecfdb00ec4c7b75bbcf7bad27711bf
Xenon000 <zchen61@u.rochester.edu>
2019-10-28 03:36:16 -0400

    draft of META, FSM, RAS

--- verilog/BRANCH_PREDICTOR.v
+++ verilog/BRANCH_PREDICTOR.v
@@ -7,16 +7,17 @@ module BRANCH_PREDICTOR (
     input STALL,
 
     // IF inputs
-    input IF_PC,
-    input IF_Instr,
+    input [31:0] IF_PC,
+    input [31:0] IF_Instr,
 
     // ID inputs
-    input ID_PC,
-    input is_Jump_Link,
-    input is_Branch,
-    input is_Taken,
+    input [31:0] ID_PC,
+    input Is_Jump_Link,
+    input Is_Branch,
+    input Is_Taken,
 
     // outputs
+    output reg flush,
     output reg take,
     output reg [31:0] alt_address
     );
---
+++ verilog/FSM.v
@@ -0,0 +1,35 @@
+module FSM (
+    input CLK,
+    input RESET,
+    input isTaken,
+    input isBranch,
+    input [31:0] InstrPC,
+
+    output reg  Pred [1023:0]
+);
+    reg  A [1023:0];
+    reg  B [1023:0];
+    wire [9:0] index;
+    assign index = InstrPC[11:2];
+
+    integer i;
+    always @(posedge CLK or negedge RESET) begin
+        if(!RESET) begin
+            for (i = 0; i < 1024; i = i + 1) begin
+                A[i] <= 0;
+                B[i] <= 0;
+                Pred[i] <= 0;
+            end
+        end else if (isBranch) begin
+            A[index] <= A[index]&B[index] | A[index]&isTaken | B[index]&isTaken;
+            B[index] <= A[index]&~B[index] | A[index]&isTaken | ~B[index]&isTaken;
+            Pred[index] <= A[index]&B[index] | A[index]&isTaken | B[index]&isTaken;
+        end
+    end
+
+    always begin
+        $display("isBranch = %x, isTaken = %x", isBranch, isTaken);
+        $display("AB = %x%x", A[index], B[index]);
+        $display("Pred = %x for %x", Pred[index], InstrPC);
+    end
+endmodule
