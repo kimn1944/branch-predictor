7800077ae44d7a7fd7cf594c240e55c7fe0243bf
Nikita Kim <kimn1944@gmail.com>
2019-10-29 20:51:28 -0400

    Submission version

--- verilog/META.v
+++ verilog/META.v
@@ -43,6 +43,7 @@ module META
     wire mispred;
     wire [31:0] Alt_PC_RAS;
     integer missnum;
+    integer branch_count;
     wire [9:0] index_IF;
     wire pred [1023:0];
     wire pred_IF;
@@ -58,11 +59,10 @@ module META
     assign pred_IF          = mispred ? 1 : (Hit_RAS ? 1 : (Hit_BTB ? (pred[index_IF] ? Pred_G : Pred_L) : 0));
     assign alt_PC_valid     = Is_Taken ? past_Alt_PC[0] == Alt_PC_ID : past_Alt_PC[0] == ID_PC + IncrementAmount;    //Is_Branch && ((past_Alt_PC[0] == Alt_PC_ID) && (Is_Taken == past_decisions_M[0]));
     assign mispred          = Is_Branch ? (alt_PC_valid ? 0 : 1) : 0;
-    assign new_pred_inst_IF = mispred ? corr_add : (Hit_RAS ? Alt_PC_RAS : (pred_IF ? (Hit_BTB ? Alt_PC_BTB : IF_PC + IncrementAmount) : IF_PC + IncrementAmount));
+    assign new_pred_inst_IF = Hit_RAS ? Alt_PC_RAS : (pred_IF ? (Hit_BTB ? Alt_PC_BTB : IF_PC + IncrementAmount) : IF_PC + IncrementAmount);
 
     // debug
-    wire [31:0] corr_add;
-    assign corr_add = Is_Taken ? Alt_PC_ID : ID_PC + 32'd8;
+    wire [31:0] corr_add = Is_Taken ? Alt_PC_ID : ID_PC + 32'd8;
 
     FSM FSM (
         .CLK(CLK),
@@ -96,6 +96,7 @@ module META
             past_decisions_G <= 0;
             past_decisions_M <= 0;
             missnum          <= 0;
+            branch_count     <= 0;
             for(i = 0; i < 7; i = i + 1) begin
                 past_IF_PC[i] <= 0;
             end
@@ -139,20 +140,22 @@ module META
                 past_IF_PC[5:0]       <= past_IF_PC[6:1];
 
             end
+            branch_count <= branch_count + Is_Branch;
             // $display("\n^^^^^^^^^^^^\n");
             // $display("____________Mispred %x Past Pred %x", mispred, past_take_ID);
             // $display("____________Take the branch_IF? %x Where to? %x", take_IF, new_pred_inst_IF);
             // $display("____________Take the branch_ID? %x Where to? %x", is_Taken_IN_ID, is_Taken_IN_ID ? Alt_PC_IN_ID : Instr_PC_IN_ID + 32'd8);
             // $display("____________Take IF %x", take_IF);
             // $display("____________Past decisions %b", past_decisions_M);
-            // $display("____________Number of Branch Misses %d", missnum);
+            $display("____________Number of Branch Misses %d", missnum);
+            $display("____________Number of branches %d", branch_count);
             // $display(" BTB: past_IF: %x               INSTR_ID: %x\nvvvvvvvvvvv\n", past_IF, Instr_PC_IN_ID);
 
             $display("********************************");
             $display("Misprediction %x", mispred);
             $display("Take          %x. Past Take     %x. Correct decision  %x", pred_IF, past_decisions_M[0], Is_Taken);
-            $display("Alt address   %x. Past Alt PC   %x. Correct address   %x", alt_address, past_Alt_PC[0], corr_add);
-            $display("BTB hit       %x. BTB_alt       %x", Hit_BTB, Alt_PC_BTB);
+            $display("Alt address   %x. Past Alt PC   %x. Correct address   %x", new_pred_inst_IF, past_Alt_PC[0], corr_add);
+            $display("BTB hit       %x", Hit_BTB);
             $display("Global        %x, Local         %x", Pred_G, Pred_L);
             $display("********************************");
         end
